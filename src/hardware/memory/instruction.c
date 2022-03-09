d909799efdaefa4311e6a0c04b0d66515aae5fa7
jupittarius <presdell@outlook.com>
2022-03-09 21:24:43 +0800

    implement push_reg mov_reg_mem

--- src/hardware/memory/instruction.c
+++ src/hardware/memory/instruction.c
@@ -67,7 +67,7 @@ static uint64_t decode_od(od_t od){
             vaddr = od.imm + *(od.reg1) + (*(od.reg2)) * od.scal;
         }
         
-        return va2pa(vaddr);
+        return vaddr;
     }
     
 }
@@ -98,6 +98,9 @@ void init_handler_table()
     handler_table[mov_reg_reg] = &mov_reg_reg_handler;
     handler_table[call] = &call_handler;
     handler_table[add_reg_reg] = &add_reg_reg_handler;
+    handler_table[push_reg] = &push_reg_handler;
+    handler_table[pop_reg] = &pop_reg_handler;
+    handler_table[mov_reg_mem] = &mov_reg_mem_handler;
 }
 
 void mov_reg_reg_handler(uint64_t src, uint64_t dst)
@@ -144,4 +147,34 @@ void add_reg_reg_handler(uint64_t src, uint64_t dst)
     */
     *(uint64_t *)dst = *(uint64_t *)dst + *(uint64_t *)src;
     reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void mov_reg_mem_handler(uint64_t src, uint64_t dst){
+    // src: reg
+    // dst: mem virtual address
+    write64bits_dram(
+        va2pa(dst),
+        *(uint64_t *)src
+    );
+
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void push_reg_handler(uint64_t src, uint64_t dst){
+    
+    // src: reg
+    // dst: empty
+    reg.rsp = reg.rsp - 0x8;
+
+    write64bits_dram(
+        va2pa(reg.rsp),
+        *(uint64_t *)src
+    );
+
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void pop_reg_handler(uint64_t src, uint64_t dst){
+    // TODO
+    printf("pop\n");
 }
